fb56f0e89cac2e1996b9148b14e55a534517e354
jaysonpye <jaysonpye@hotmail.com>
2026-02-09 14:11:15 +0900

    work style section

--- index.php
+++ index.php
@@ -16,10 +16,13 @@ include __DIR__ . '/includes/header.php';
     <div class="about-body">
       <div class="about-text ku-text-glow">
         <p>
-          「KidsUP」は学童保育型英会話スクールです。<br>
-          子供たちの英語力はもちろん、社会で活躍するための自信と
-          総合的な人間力を育みます。保護者様に安心していただける
-          サービス環境と、高品質なカリキュラムでお子様の成長をサポートします。
+          <span class="about-text__line">「KidsUP」は学童保育型英会話スクールです。</span>
+          <br class="about-text__break">
+          <span class="about-text__line">子供たちの英語力はもちろん、社会で活躍するための自信と</span>
+          <br class="about-text__break">
+          <span class="about-text__line">総合的な人間力を育みます。保護者様に安心していただける</span>
+          <br class="about-text__break">
+          <span class="about-text__line">サービス環境と、高品質なカリキュラムでお子様の成長をサポートします。</span>
         </p>
       </div>
       <div class="about-media">
@@ -52,6 +55,49 @@ include __DIR__ . '/includes/header.php';
         </ul>
       </div>
     </details>
+  </div>
+</section>
+<section class="ku-section ku-section--white stats-section">
+  <div class="ku-section__inner">
+    <header class="about-header">
+      <div class="ku-section-heading ku-section-heading--image ku-section-heading--orange">
+        <img class="ku-section-heading__image" src="images/work_style.png" alt="Work style">
+        <p class="ku-section-heading__subtitle">働く魅力</p>
+        <span class="ku-section-heading__line" aria-hidden="true"></span>
+      </div>
+    </header>
+    <div class="stats-row">
+      <div class="stats-row__header">
+        <div class="stats-header">
+          <div class="stats-header__media">
+            <img class="stats-header__image" src="images/01.png" alt="01">
+            <div class="stats-header__text">
+              <p class="stats-header__title">スクール売り上げ</p>
+              <p class="stats-header__meta">売上：25.2億（2025年 2月期）</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="stats-row__graph">
+        <img src="images/graph-1.png" alt="スクール売り上げグラフ">
+      </div>
+    </div>
+    <div class="stats-row stats-row--reverse">
+      <div class="stats-row__header">
+        <div class="stats-header">
+          <div class="stats-header__media">
+            <img class="stats-header__image" src="images/02.png" alt="02">
+            <div class="stats-header__text">
+              <p class="stats-header__title">スクール数</p>
+              <p class="stats-header__meta">スクール数：36校（2025年11月時点）</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="stats-row__graph">
+        <img src="images/graph-2.png" alt="スクール数グラフ">
+      </div>
+    </div>
   </div>
 </section>
 <?php include __DIR__ . '/includes/footer.php'; ?>
